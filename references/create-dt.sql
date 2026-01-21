f0205e71cf7a669032c281c1202957eee8cec2f4
Kris Jenkins <kris.jenkins@snowflake.com>
2026-01-21 14:58:01 +0000

    Consolidating the two dynamic tables files.
    I'm not convinced the separation is worth the extra effort. In a lab,
    every file is an extra ten minutes of making sure we're all finished.

--- references/create-dt.sql
+++ references/create-dt.sql
@@ -3,10 +3,6 @@ use role accountadmin;
 use warehouse compute_wh;
 use database analytics_db;
 
--- Display what type of data and example data in the customers table
-desc table raw_db.public.customers;
-select * from raw_db.public.customers;
-
 -- Create a dynamic table for customers table
 create or replace dynamic table stg_customers_dt
     target_lag=downstream
@@ -32,10 +28,28 @@ create or replace dynamic table stg_orders_dt
         purchase:"quantity"::number(5) as quantity,
         purchase:"purchase_date"::date as order_date
     from raw_db.public.orders;
-        
+
 -- Query the new DT that you created
 select * from analytics_db.public.stg_customers_dt;
 select * from analytics_db.public.stg_orders_dt;
+
+-- Create a fact dynamic table for customer orders
+create or replace dynamic table fct_customer_orders_dt
+    target_lag=downstream
+    warehouse=compute_wh
+    as select 
+        c.customer_id,
+        c.customer_name,
+        o.product_id,
+        o.order_price,
+        o.quantity,
+        o.order_date
+    from stg_customers_dt c
+    left join stg_orders_dt o
+        on c.customer_id = o.customer_id;
+
+-- Query the new Fact DT that you created
+select * from analytics_db.public.fct_customer_orders_dt;
 
 -- Displays all the dynamic tables that we have created
 show dynamic tables;
